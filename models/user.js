a9e82517742e55c6c0725e7fcdcad7b256467f80
Benicen <Benicensoto@Gmail.com>
2025-02-03 18:52:38 -0500

    email and password added

--- models/user.js
+++ models/user.js
@@ -18,6 +18,38 @@ const userSchema = new mongoose.Schema({
         message: "You must enter a valid URL",
     },
   },
+  email: {
+    type: String,
+    required: [true, "The email field is requred."],
+    validate: {
+        validator(value) {
+            return validator.isURL(value);
+        },
+        message: "You must enter a valid email",
+    },
+  },
+  password: {
+    type: String,
+    required: [true, "The password field is requred."],
+  },
 });
+
+userSchema.statics.findUserByCredentials = function findUserByCredentials(
+  email,
+  password
+) {
+  return this.findOne({ email }).then((user) => {
+    if (!user) {
+      return Promise.reject(new Error("Incorrect email or password"));
+    }
+
+    return bcrypt.compare(password, user.password).then((matched) => {
+      if (!matched) {
+        return Promise.reject(new Error("Incorrect email or password"));
+      }
+      return user;
+    });
+  });
+};
 
 module.exports = mongoose.model("user", userSchema);
