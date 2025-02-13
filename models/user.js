0e54877e41ea751ee97b87e3895645efb44d7961
Benicen <Benicensoto@Gmail.com>
2025-02-13 07:29:16 -0500

    changes

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
