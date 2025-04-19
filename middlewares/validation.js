4a746a1cd5af511aa458ba3187468c2be55776b2
Benicen <Benicensoto@Gmail.com>
2025-04-19 18:59:47 -0400

    username updated

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -27,7 +27,7 @@ module.exports.validateCardBody = celebrate({
 // 2. Validate User Info Body (Create User)
 module.exports.validateUser = celebrate({
   body: Joi.object().keys({
-    username: Joi.string().required().min(2).max(30).messages({
+    name: Joi.string().required().min(2).max(30).messages({
       "string.min": 'The minimum length of the "username" field is 2',
       "string.max": 'The maximum length of the "username" field is 30',
       "string.empty": 'The "username" field must be filled in',
